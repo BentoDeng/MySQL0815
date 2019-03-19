85e28e12b3b847b3b1d8f868b0d69320e5266f5b
Bentrix <48341925+DepersonalizedMan@users.noreply.github.com>
2019-03-19 16:12:47 +0800

    Add files via upload

---
+++ multofunctional query.sql
@@ -0,0 +1,18 @@
+SELECT 
+  90 * 1098 AS 结果 ;
+
+SELECT 
+  last_name 姓氏 ,
+  first_name 名字
+FROM
+  employees ;
+
+SELECT 
+  salary AS 'output' 
+FROM
+  employees ;
+
+SELECT 
+  salary AS "output"
+FROM
+  employees ;
